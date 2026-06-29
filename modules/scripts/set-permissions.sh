fdc97f31b5962cbc78f2f61196ccb421446e5ba9
Azzy Novais <floatingfloatskies@protonmail.com>
2026-06-29 15:35:22 -0400

    Add files via upload

--- modules/scripts/set-permissions.sh
+++ modules/scripts/set-permissions.sh
@@ -11,3 +11,6 @@ chmod 644 /etc/sysctl.d/80-anubis-hardening.conf
 chmod 755 /usr/share/anubis-os/scripts/anubis-pick-wallpaper.sh
 chmod 644 /usr/lib/systemd/system/anubis-first-boot-wallpaper.service
 chmod 644 /usr/share/backgrounds/anubis-os/*
+
+# GNOME extensions default-enable dconf override
+chmod 644 /etc/dconf/db/local.d/00-anubis-extensions
