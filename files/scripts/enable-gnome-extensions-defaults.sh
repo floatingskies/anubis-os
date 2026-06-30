6197143dccbb244d07153cf73b046a838a0b910c
Azzy Novais <floatingfloatskies@protonmail.com>
2026-06-30 13:27:22 -0400

    Update gnome extensions configuration in script

--- files/scripts/enable-gnome-extensions-defaults.sh
+++ files/scripts/enable-gnome-extensions-defaults.sh
@@ -1,37 +1,29 @@
 #!/usr/bin/env bash
 set -euo pipefail
 trap 'echo "[enable-gnome-extensions-defaults] FAILED at line $LINENO" >&2' ERR
-
 PROFILE=/etc/dconf/profile/user
 mkdir -p /etc/dconf/profile
-
 if [[ ! -f "$PROFILE" ]]; then
     printf 'user-db:user\nsystem-db:local\n' > "$PROFILE"
 elif ! grep -q '^system-db:local$' "$PROFILE"; then
     echo 'system-db:local' >> "$PROFILE"
 fi
-
 mkdir -p /etc/dconf/db/local.d
-
 cat > /etc/dconf/db/local.d/00-anubis-extensions << 'DCONF'
 [org/gnome/shell]
 enabled-extensions=['dash-to-dock@micxgx.gmail.com', 'appindicatorsupport@rgcjonas.gmail.com', 'blur-my-shell@aunetx', 'logomenu@aryan_k', 'caffeine@patapon.info', 'paperwm@paperwm.github.com']
 disable-user-extensions=false
-
-[org/gnome/shell/extensions/logomenu]
-logo-icon-system-name=''
-menu-button-icon='Custom_Image'
-custom-icon='/usr/share/pixmaps/anubis-logo.png'
-
+[org/gnome/shell/extensions/logo-menu]
+menu-button-icon-image='Custom_Image'
+custom-icon-path='/usr/share/pixmaps/anubis-logo.png'
+icon-size=36
 [org/gnome/desktop/background]
 picture-uri='file:///usr/share/backgrounds/anubis-os/anubis-04-jonesy-lake.png'
 picture-uri-dark='file:///usr/share/backgrounds/anubis-os/anubis-04-jonesy-lake.png'
 picture-options='zoom'
-
 [org/gnome/desktop/screensaver]
 picture-uri='file:///usr/share/backgrounds/anubis-os/anubis-04-jonesy-lake.png'
 picture-options='zoom'
 DCONF
-
 dconf update 2>/dev/null || true
 echo "[enable-gnome-extensions-defaults] All done."
